18fc5e483df7fda566c0551089cf3137b02c6c3b
Ye Wenbin <yewenbin@winwin-inc.com>
2021-09-10 17:25:33 +0800

    fix worker exit timeout

---
+++ swoole/src/listener/WorkerExitEventListener.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace kuiper\swoole\listener;
+
+use kuiper\event\EventListenerInterface;
+use kuiper\swoole\event\WorkerExitEvent;
+use Swoole\Timer;
+
+class WorkerExitEventListener implements EventListenerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __invoke($event): void
+    {
+        Timer::clearAll();
+    }
+
+    public function getSubscribedEvent(): string
+    {
+        return WorkerExitEvent::class;
+    }
+}
